97c1f9877f37ca5c1f403e051fddd23cdde63b4b
Jason Chlus <33561386+jchlus@users.noreply.github.com>
2020-04-06 21:02:34 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,216 @@
+var request = require('request');
+
+const chalk  = require('chalk');
+
+var urlRoot = "https://api.github.com";
+// NCSU Enterprise endpoint:
+//var urlRoot = "https://api.github.ncsu.edu";
+
+var userId = "jchlus";
+var config = {};
+// Retrieve our api token from the environment variables.
+config.token = process.env.GITHUBTOKEN;
+
+if( !config.token )
+{
+	console.log(chalk`{red.bold GITHUBTOKEN is not defined!}`);
+	console.log(`Please set your environment variables with appropriate token.`);
+	console.log(chalk`{italic You may need to refresh your shell in order for your changes to take place.}`);
+	process.exit(1);
+}
+
+console.log(chalk.green(`Your token is: ${config.token.substring(0,4)}...`));
+
+if (process.env.NODE_ENV != 'test')
+{
+	(async () => {
+		await listAuthenicatedUserRepos();
+		await listBranches(userId, "HW4-345-repo");
+		await createRepo(userId, "newrepo");
+		await createIssue(userId, "HW4-345-repo", "newissue");
+		await enableWikiSupport(userId, "HW4-345-repo");
+
+	})()
+}
+
+function getDefaultOptions(endpoint, method)
+{
+	var options = {
+		url: urlRoot + endpoint,
+		method: method,
+		headers: {
+			"User-Agent": "ssw345-REST-lab",
+			"content-type": "application/json",
+			"Authorization": `token ${config.token}`
+		}
+	};
+	return options;
+}
+
+async function getUser()
+{
+	let options = getDefaultOptions("/user", "GET");
+
+	// Send a http request to url and specify a callback that will be called upon its return.
+	return new Promise(function(resolve, reject)
+	{
+		request(options, function (error, response, body) {
+
+			resolve( JSON.parse(body).login );
+		});
+	});
+}
+
+function listAuthenicatedUserRepos()
+{
+	let options = getDefaultOptions("/user/repos?visibility=all", "GET");
+
+	// Send a http request to url and specify a callback that will be called upon its return.
+	return new Promise(function(resolve, reject)
+	{
+		request(options, function (error, response, body) 
+		{
+			if( error )
+			{
+				console.log( chalk.red( error ));
+				reject(error);
+				return; // Terminate execution.
+			}
+
+			var obj = JSON.parse(body);
+			for( var i = 0; i < obj.length; i++ )
+			{
+				var name = obj[i].name;
+				console.log( name );
+			}
+
+			// Return object for people calling our method.
+			resolve( obj );
+		});
+	});
+
+}
+
+// 1. Write code for listBranches in a given repo under an owner. See list branches
+async function listBranches(owner,repo)
+{
+	let options = getDefaultOptions(`/repos/${owner}/${repo}/branches`, "GET");
+
+	// Send a http request to url and specify a callback that will be called upon its return.
+	return new Promise(function(resolve, reject)
+	{
+		request(options, function (error, response, body) {
+
+			if( error )
+			{
+				console.log( chalk.red( error ));
+				reject(error);
+				return; // Terminate execution.
+			}
+
+			var obj = JSON.parse(body);
+			for( var i = 0; i < obj.length; i++ )
+			{
+				var name = obj[i].name;
+				console.log( name );
+			}
+			// console.debug( options );
+			resolve( JSON.parse(body) );
+
+		});
+	});
+}
+
+// 2. Write code to create a new repo
+async function createRepo(owner,repo)
+{
+	let options = getDefaultOptions("/users/repos", "POST");
+
+	// Send a http request to url and specify a callback that will be called upon its return.
+	return new Promise(function(resolve, reject)
+	{
+		request(options, function (error, response, body) {
+
+			if( error )
+			{
+				console.log( chalk.red( error ));
+				reject(error);
+				return; // Terminate execution.
+			}
+
+			var statusCode = JSON.parse(body);
+
+			if(statusCode == 200) {
+				console.log(chalk.green('\n' + JSON.name + 'created successfully '));
+			}
+
+			resolve( response.statusCode );
+
+		});
+	});
+
+}
+// 3. Write code for creating an issue for an existing repo.
+async function createIssue(owner, repo, issueName, issueBody)
+{
+	let options = getDefaultOptions(`/repos/${owner}/${repo}/issues`, "POST");
+
+	// Send a http request to url and specify a callback that will be called upon its return.
+	return new Promise(function(resolve, reject)
+	{
+		request(options, function (error, response, body) {
+			
+			if( error )
+			{
+				console.log( chalk.red( error ));
+				reject(error);
+				return; // Terminate execution.
+			}
+
+			var statusCode = JSON.parse(body);
+
+			if(statusCode == 200) {
+				console.log(chalk.green('\n' + JSON.name + 'created successfully '));
+			}
+
+			resolve( response.statusCode );
+
+		});
+	});
+}
+
+// 4. Write code for editing a repo to enable wiki support.
+async function enableWikiSupport(owner,repo)
+{
+	let options = getDefaultOptions(`/repos/${owner}/${repo}/`, "PATCH");
+
+	// Send a http request to url and specify a callback that will be called upon its return.
+	return new Promise(function(resolve, reject)
+	{
+		request(options, function (error, response, body) {
+
+			if( error )
+			{
+				console.log( chalk.red( error ));
+				reject(error);
+				return; // Terminate execution.
+			}
+
+			var obj = JSON.parse(body);
+			for( var i = 0; i < obj.length; i++ )
+			{
+				var name = obj[i].name;
+				console.log( name );
+			}
+
+			resolve( JSON.parse(body) );
+		});
+	});	
+}
+
+module.exports.getUser = getUser;
+module.exports.listAuthenicatedUserRepos = listAuthenicatedUserRepos;
+module.exports.listBranches = listBranches;
+module.exports.createRepo = createRepo;
+module.exports.createIssue = createIssue;
+module.exports.enableWikiSupport = enableWikiSupport;
